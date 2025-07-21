5d60f36e83c289b31cd593f6519ed1a84cafc1e2
ToluwaniOladeji <t.oladeji@alustudent.com>
2025-07-21 15:33:12 +0200

    Added the register_newsite Function

--- cli/upload_site.py
+++ cli/upload_site.py
@@ -2,3 +2,23 @@ from models.tourist_site import Site
 from db import SessionLocal
 
 
+def register_newsite():
+    session = SessionLocal()
+    try:
+        print("Register New Site")
+        sitename = input("Site Name: ")
+        location = input("Location: ")
+        gatepass = input("Gatepass: ")
+
+        user = Site.create_touristsite(sitename, location, gatepass,)
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
+        print("\n✅ Site Added Successfully!")
+        print(user)
+    except Exception as e:
+        session.rollback()
+        print("Error Adding New Site:", e)
+    finally:
+        session.close()
