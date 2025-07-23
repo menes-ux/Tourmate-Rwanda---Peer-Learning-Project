0a71a4730812daa5090782588719c7bf56b2c418
divine150-coder <d.mutesi1@alustudent.com>
2025-07-23 18:12:35 +0200

    I created a class that adds user field

---
+++ models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
+import uuid
+
+from db import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    fullname = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    country = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
