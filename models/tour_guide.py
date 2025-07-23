93f8ebefc49bc4547c73a9f8a524104518798925
nakuwa23 <b.nakuwa@alustudent.com>
2025-07-23 18:36:19 +0200

    This is a class that creates and add the tour_guide table to the main database

---
+++ models/tour_guide.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
+import uuid
+from  db import Base
+
+class TourGuide(Base):
+    _tablename_ = "tour_guides"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    fullname = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    country = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
