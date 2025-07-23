dbdf9ef75c43102f4b4a29eefdf51062c89c9b23
nirere123 <n.sayinzoga@alustudent.com>
2025-07-23 05:19:55 -1000

    created a class that add adds cab information to the data base

---
+++ models/cab.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
+import uuid
+from  db import Base
+
+class Cab(Base):
+    _tablename_ = 'cabs'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    cab_number = Column(String, nullable=False, unique=True)
+    driver_name = Column(String, nullable=False)
+    driver_contact = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+    
