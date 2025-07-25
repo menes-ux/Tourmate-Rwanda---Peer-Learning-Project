93eb9bc85ab2183b89f7cd8c28a21a3d94404ccf
EucTech <euccreative@gmail.com>
2025-07-25 17:29:42 -0400

    updated register user file

--- cli/register_user.py
+++ cli/register_user.py
@@ -1,8 +1,13 @@
 from models.user import User
 from db import SessionLocal
 
+session = SessionLocal()
+
+
 def register_user():
-    session = SessionLocal()
+    """
+    This is a function that registers a new user in the system.
+    """
     try:
         print(" Register New User")
         fullname = input("Full Name: ").strip()
@@ -22,5 +27,27 @@ def register_user():
     except Exception as e:
         session.rollback()
         print("Error creating user:", e)
+    finally:
+        session.close()
+        
+        
+def login_user():
+    """
+        This is a function that prompts the user to log in with their email and password
+    """
+    try:
+        print("Login To Your Account")
+        print("Please enter your email and password to log in.")
+        print()
+        
+        email = input("Email: ").strip()
+        password = input("Password: ").strip()
+        user = session.query(User).filter_by(email=email, password=password).first()
+        if user:
+            print(f"Thank you for logging in, {user.fullname}!")
+            return user
+        else:
+            print("Check your email and password. User not found.")
+            return None
     finally:
         session.close()
